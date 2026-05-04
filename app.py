9137192250a56f10f354ef626dcf91f5e0edcf2e
Ana Beatriz <anabeatriz.arruda7@gmail.com>
2026-05-04 14:22:48 -0300

    feat: adiciona criação de tarefas com formulário

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,11 +6,24 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 class Tasks(db.Model):
-    id = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), unique=True, nullable=False)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    tasks = Tasks.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/create', methods=["POST"])
+def create_task():
+    description = request.form['description']
+    new_task = Tasks(description=description)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect('/')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
